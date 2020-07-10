10820fe45d42c73cdbc38d24bb19f01c21520b20
wanghui219 <274244485@qq.com>
2020-07-10 15:30:35 +0800

    添加了55555一行

--- app/src/main/java/com/jy/gittest/MainActivity.java
+++ app/src/main/java/com/jy/gittest/MainActivity.java
@@ -15,9 +15,8 @@ public class MainActivity extends AppCompatActivity {
         String s = "111";
 		String x = "444";
         String n = "3333";
-        String m = "555555";
-        String a1 = "66666";
-        String a2 = "777";
+        String b = "555555";
+
 
     }
 }
